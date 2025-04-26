0190e4e969c8562333ccc964fb6a1220c1120d4c
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-04-26 20:49:28 +0330

    Update ZarinPal.kt
    Enhanced and organized.

--- src/main/java/com/example/zarinpal/ZarinPal.kt
+++ src/main/java/com/example/zarinpal/ZarinPal.kt
@@ -23,37 +23,28 @@ import com.example.zarinpal.utils.Validator
  * ZarinPal class handles all interactions with the ZarinPal API.
  * Provides methods for creating payments, verifying payments, refunds, reversals, and more.
  */
-class ZarinPal(config: Config) {
+class ZarinPal(private val config: Config) {
 
-    // Instance of PaymentService used for API calls
     private val service = PaymentService.create(config)
 
-    // Configuration object containing sandbox mode and other settings
-    private val config = config
-
-    /**
-     * Creates a new payment and returns the response.
-     * @param paymentRequest Request data for creating the payment.
-     * @param redirectUrl Callback function to handle the payment gateway URL and status.
-     * @return [CreatePaymentDataResponse] containing details of the created payment.
-     */
     suspend fun createPayment(
         paymentRequest: CreatePaymentRequest,
         redirectUrl: (paymentGatewayUri: String, status: Int) -> Unit
     ): CreatePaymentDataResponse? {
-        Validator.validateMerchantId(paymentRequest.merchantId ?: config.merchantId)
-        Validator.validateCallbackUrl(paymentRequest.callbackUrl)
-        Validator.validateAmount(paymentRequest.amount)
-        Validator.validateMobile(paymentRequest.metadata?.mobile)
-        Validator.validateEmail(paymentRequest.metadata?.email)
-        Validator.validateCardPan(paymentRequest.cardPan)
+        with(Validator) {
+            validateMerchantId(paymentRequest.merchantId ?: config.merchantId)
+            validateCallbackUrl(paymentRequest.callbackUrl)
+            validateAmount(paymentRequest.amount)
+            validateMobile(paymentRequest.metadata?.mobile)
+            validateEmail(paymentRequest.metadata?.email)
+            validateCardPan(paymentRequest.cardPan)
+        }
 
-        val paymentResponse =
-            service.createPayment(paymentRequest)
+        val paymentResponse = service.createPayment(paymentRequest)
 
         val paymentGatewayUri = HttpRoutes.getRedirectUrl(
             sandBox = paymentRequest.sandBox ?: config.sandBox,
-            authority = paymentResponse?.authority ?: ""
+            authority = paymentResponse?.authority.orEmpty()
         )
 
         redirectUrl(paymentGatewayUri, paymentResponse?.code ?: 0)
@@ -61,89 +52,58 @@ class ZarinPal(config: Config) {
         return paymentResponse
     }
 
-    /**
-     * Verifies a payment using the authority code.
-     * @param paymentVerifyRequest Request data for verifying the payment.
-     * @return [PaymentVerificationDataResponse] containing verification details.
-     */
-    suspend fun paymentVerify(
-        paymentVerifyRequest: PaymentVerifyRequest
-    ): PaymentVerificationDataResponse? {
-        Validator.validateMerchantId(paymentVerifyRequest.merchantId ?: config.merchantId)
-        Validator.validateAuthority(paymentVerifyRequest.authority)
-        Validator.validateAmount(paymentVerifyRequest.amount)
+    suspend fun paymentVerify(paymentVerifyRequest: PaymentVerifyRequest): PaymentVerificationDataResponse? {
+        with(Validator) {
+            validateMerchantId(paymentVerifyRequest.merchantId ?: config.merchantId)
+            validateAuthority(paymentVerifyRequest.authority)
+            validateAmount(paymentVerifyRequest.amount)
+        }
 
         return service.paymentVerify(paymentVerifyRequest)
     }
 
-    /**
-     * Inquires about a payment status.
-     * @param paymentInquiryRequest Request data for the payment inquiry.
-     * @return [PaymentInquiryDataResponse] containing inquiry details.
-     */
-    suspend fun paymentInquiry(
-        paymentInquiryRequest: PaymentInquiryRequest
-    ): PaymentInquiryDataResponse? {
-        Validator.validateMerchantId(paymentInquiryRequest.merchantId ?: config.merchantId)
-        Validator.validateAuthority(paymentInquiryRequest.authority)
+    suspend fun paymentInquiry(paymentInquiryRequest: PaymentInquiryRequest): PaymentInquiryDataResponse? {
+        with(Validator) {
+            validateMerchantId(paymentInquiryRequest.merchantId ?: config.merchantId)
+            validateAuthority(paymentInquiryRequest.authority)
+        }
 
         return service.paymentInquiry(paymentInquiryRequest)
     }
 
-
-    /**
-     * Retrieves unverified payments.
-     * @param paymentUnVerifiedRequest Optional request data for retrieving unverified payments.
-     * @return [PaymentUnVerifiedDataResponse] containing unverified payment details.
-     */
     suspend fun paymentUnVerified(
         paymentUnVerifiedRequest: PaymentUnVerifiedRequest = PaymentUnVerifiedRequest()
     ): PaymentUnVerifiedDataResponse? {
         Validator.validateMerchantId(paymentUnVerifiedRequest.merchantId ?: config.merchantId)
-
         return service.paymentUnVerified(paymentUnVerifiedRequest)
     }
 
-    /**
-     * Retrieves unverified payments.
-     * @param paymentUnVerifiedRequest Optional request data for retrieving unverified payments.
-     * @return [PaymentUnVerifiedDataResponse] containing unverified payment details.
-     */
-    suspend fun paymentReverse(
-        paymentReverseRequest: PaymentReverseRequest
-    ): PaymentReverseDataResponse? {
-        Validator.validateMerchantId(paymentReverseRequest.merchantId ?: config.merchantId)
-        Validator.validateAuthority(paymentReverseRequest.authority)
+    suspend fun paymentReverse(paymentReverseRequest: PaymentReverseRequest): PaymentReverseDataResponse? {
+        with(Validator) {
+            validateMerchantId(paymentReverseRequest.merchantId ?: config.merchantId)
+            validateAuthority(paymentReverseRequest.authority)
+        }
 
         return service.paymentReverse(paymentReverseRequest)
     }
 
-    /**
-     * Retrieves transaction history.
-     * @param transactionRequest Request data for fetching transactions.
-     * @return A list of [Session] objects containing transaction details.
-     */
-    suspend fun getTransactions(
-        transactionRequest: TransactionRequest
-    ): List<Session>? {
-        Validator.validateTerminalId(transactionRequest.terminalId)
-        Validator.validateLimit(transactionRequest.limit)
-        Validator.validateOffset(transactionRequest.offset)
+    suspend fun getTransactions(transactionRequest: TransactionRequest): List<Session>? {
+        with(Validator) {
+            validateTerminalId(transactionRequest.terminalId)
+            validateLimit(transactionRequest.limit)
+            validateOffset(transactionRequest.offset)
+        }
 
         return service.getTransactions(transactionRequest)
     }
 
-    /**
-     * Processes a payment refund.
-     * @param paymentRefundRequest Request data for refunding the payment.
-     * @return [PaymentRefundResponse] containing refund details.
-     */
-    suspend fun paymentRefund(
-        paymentRefundRequest: PaymentRefundRequest
-    ): PaymentRefundResponse? {
-        Validator.validateSessionId(paymentRefundRequest.sessionId)
-        Validator.validateAmount(paymentRefundRequest.amount, minAmount = 20_000)
+    suspend fun paymentRefund(paymentRefundRequest: PaymentRefundRequest): PaymentRefundResponse? {
+        with(Validator) {
+            validateSessionId(paymentRefundRequest.sessionId)
+            validateAmount(paymentRefundRequest.amount, minAmount = 20_000)
+        }
 
         return service.paymentRefund(paymentRefundRequest)
     }
 }
+ 
